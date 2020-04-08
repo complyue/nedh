a346adaf2231ed8484fac5c16742530977b143e7
ComplYue <compl.yue@icloud.com>
2020-04-08 21:13:23 +0800

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nedh - Networked Edh
---
+++ nedh/pub/Language/Edh/Net.hs
@@ -0,0 +1,50 @@
+
+module Language.Edh.Net
+  ( installNetBatteries
+  -- TODO organize and doc the re-exports
+  , module Language.Edh.Net.MicroProto
+  , module Language.Edh.Net.Peer
+  , module Language.Edh.Net.Server
+  )
+where
+
+import           Prelude
+-- import           Debug.Trace
+
+import           Control.Monad.Reader
+
+import           Language.Edh.Net.MicroProto
+import           Language.Edh.Net.Peer
+import           Language.Edh.Net.Server
+
+import           Language.Edh.EHI
+
+
+installNetBatteries :: EdhWorld -> IO ()
+installNetBatteries !world =
+
+  void $ installEdhModule world "net" $ \pgs modu -> do
+
+    let ctx       = edh'context pgs
+        moduScope = objectScope ctx modu
+
+    !moduArts <-
+      sequence
+        $ [ (AttrByName nm, ) <$> mkHostProc moduScope mc nm hp args
+          | (mc, nm, hp, args) <-
+            [ 
+              -- ( EdhMethod
+              -- , "serveClients"
+              -- , serveClientsProc
+              -- , PackReceiver
+              --   [ RecvArg "bindLocalAddr" (Just (EdhString "127.0.0.1")) Nothing
+              --   , RecvArg "bindLocalPort"
+              --             (Just (EdhDecimal $ fromIntegral 3721))
+              --             Nothing
+              --   ]
+              -- )
+            ]
+          ]
+
+    updateEntityAttrs pgs (objEntity modu) moduArts
+
