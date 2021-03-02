8f9a4c356ee99ee46b671a3d4661e3b5fd97c24e
ComplYue <compl.yue@icloud.com>
2021-03-03 01:00:51 +0800

    fixup

--- host.hs/src/Language/Edh/Net.hs
+++ host.hs/src/Language/Edh/Net.hs
@@ -1,74 +1,71 @@
-
 module Language.Edh.Net
-  ( installNetBatteries
-  -- TODO organize and doc the re-exports
-  , module Language.Edh.Net.MicroProto
-  , module Language.Edh.Net.Addr
-  , module Language.Edh.Net.Peer
-  , module Language.Edh.Net.Server
-  , module Language.Edh.Net.Client
+  ( installNetBatteries,
+    -- TODO organize and doc the re-exports
+    module Language.Edh.Net.MicroProto,
+    module Language.Edh.Net.Addr,
+    module Language.Edh.Net.Peer,
+    module Language.Edh.Net.Server,
+    module Language.Edh.Net.Client,
   )
 where
 
-import           Prelude
 -- import           Debug.Trace
 
-import           Control.Monad.Reader
-
-import           Language.Edh.EHI
-
-import           Language.Edh.Net.MicroProto
-import           Language.Edh.Net.Peer
-import           Language.Edh.Net.Addr
-import           Language.Edh.Net.Server
-import           Language.Edh.Net.Client
-import           Language.Edh.Net.WebSocket
-import           Language.Edh.Net.Http
-import           Language.Edh.Net.Sniffer
-import           Language.Edh.Net.Advertiser
-
+import Control.Monad.Reader
+import Language.Edh.EHI
+import Language.Edh.Net.Addr
+import Language.Edh.Net.Advertiser
+import Language.Edh.Net.Client
+import Language.Edh.Net.Http
+import Language.Edh.Net.MicroProto
+import Language.Edh.Net.Peer
+import Language.Edh.Net.Server
+import Language.Edh.Net.Sniffer
+import Language.Edh.Net.WebSocket
+import Prelude
 
 installNetBatteries :: EdhWorld -> IO ()
 installNetBatteries !world =
+  void $
+    installEdhModule world "net/RT" $ \ !ets exit -> do
+      let !moduScope = contextScope $ edh'context ets
 
-  void $ installEdhModule world "net/RT" $ \ !ets exit -> do
+      !peerClass <- createPeerClass moduScope
+      !addrClass <- createAddrClass moduScope
 
-    let !moduScope = contextScope $ edh'context ets
+      !serverClass <- createServerClass consoleWarn addrClass peerClass moduScope
+      !clientClass <- createClientClass consoleWarn addrClass peerClass moduScope
 
-    !peerClass <- createPeerClass moduScope
-    !addrClass <- createAddrClass moduScope
+      !wsServerClass <-
+        createWsServerClass
+          consoleWarn
+          addrClass
+          peerClass
+          moduScope
+      !httpServerClass <- createHttpServerClass addrClass moduScope
 
-    !serverClass <- createServerClass consoleWarn addrClass peerClass moduScope
-    !clientClass <- createClientClass consoleWarn addrClass peerClass moduScope
+      !snifferClass <- createSnifferClass addrClass moduScope
+      !advertiserClass <- createAdvertiserClass addrClass moduScope
 
-    !wsServerClass <- createWsServerClass consoleWarn
-                                          addrClass
-                                          peerClass
-                                          moduScope
-    !httpServerClass <- createHttpServerClass addrClass moduScope
+      let !moduArts =
+            [ ("Peer", EdhObject peerClass),
+              ("Addr", EdhObject addrClass),
+              ("Server", EdhObject serverClass),
+              ("Client", EdhObject clientClass),
+              ("WsServer", EdhObject wsServerClass),
+              ("HttpServer", EdhObject httpServerClass),
+              ("Sniffer", EdhObject snifferClass),
+              ("Advertiser", EdhObject advertiserClass)
+            ]
+      !artsDict <-
+        EdhDict
+          <$> createEdhDict [(EdhString k, v) | (k, v) <- moduArts]
+      flip iopdUpdate (edh'scope'entity moduScope) $
+        [(AttrByName k, v) | (k, v) <- moduArts]
+          ++ [(AttrByName "__exports__", artsDict)]
 
-    !snifferClass    <- createSnifferClass addrClass moduScope
-    !advertiserClass <- createAdvertiserClass addrClass moduScope
-
-    let !moduArts =
-          [ ("Peer"      , EdhObject peerClass)
-          , ("Addr"      , EdhObject addrClass)
-          , ("Server"    , EdhObject serverClass)
-          , ("Client"    , EdhObject clientClass)
-          , ("WsServer"  , EdhObject wsServerClass)
-          , ("HttpServer", EdhObject httpServerClass)
-          , ("Sniffer"   , EdhObject snifferClass)
-          , ("Advertiser", EdhObject advertiserClass)
-          ]
-    !artsDict <- EdhDict
-      <$> createEdhDict [ (EdhString k, v) | (k, v) <- moduArts ]
-    flip iopdUpdate (edh'scope'entity moduScope)
-      $  [ (AttrByName k, v) | (k, v) <- moduArts ]
-      ++ [(AttrByName "__exports__", artsDict)]
-
-    exit
-
- where
-  !worldLogger = consoleLogger $ edh'world'console world
-  consoleWarn !msg =
-    worldLogger 30 (Just "<nedh>") (ArgsPack [EdhString msg] odEmpty)
+      exit
+  where
+    !worldLogger = consoleLogger $ edh'world'console world
+    consoleWarn !msg =
+      worldLogger 30 (Just "<nedh>") msg
