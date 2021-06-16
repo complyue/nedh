1b0f18d1cb382a344411041829bcb626b5aa92ee
ComplYue <compl.yue@icloud.com>
2021-06-16 22:03:08 +0800

    repl upd

--- host.hs/repl/Main.hs
+++ host.hs/repl/Main.hs
@@ -1,47 +1,46 @@
-
 module Main where
 
-import           Prelude
 -- import           Debug.Trace
 
-import           System.Environment
-import           System.IO
-import           System.Exit
-
-import           Control.Monad
-import           Control.Exception
-import           Control.Concurrent
-
-import qualified Data.Text                     as T
-
-import           Language.Edh.EHI
-
-import           Repl
-
+import Control.Concurrent
+import Control.Exception
+import Control.Monad (void)
+import qualified Data.Text as T
+import GHCi.Signals
+import Language.Edh.EHI
+import Repl
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+import Prelude
 
 main :: IO ()
-main = getArgs >>= \case
-  []        -> runModu "net"
-  [edhModu] -> runModu edhModu
-  _         -> hPutStrLn stderr "Usage: nedh [ <edh-module> ]" >> exitFailure
- where
-  runModu :: FilePath -> IO ()
-  runModu !moduSpec = do
-    !console <- defaultEdhConsole defaultEdhConsoleSettings
-    let !consoleOut = consoleIO console . ConsoleOut
+main = do
+  -- don't crash on double Ctrl^C or Ctrl^\, mimic what GHCi is doing
+  installSignalHandlers
+  -- run the specified module, assuming `repl` semantics
+  getArgs >>= \case
+    [] -> runModu "net"
+    [edhModu] -> runModu edhModu
+    _ -> hPutStrLn stderr "Usage: nedh [ <edh-module> ]" >> exitFailure
+  where
+    runModu :: FilePath -> IO ()
+    runModu !moduSpec = do
+      !console <- defaultEdhConsole defaultEdhConsoleSettings
+      let !consoleOut = consoleIO console . ConsoleOut
 
-    void $ forkFinally (edhProgLoop moduSpec console) $ \ !result -> do
-      case result of
-        Left (e :: SomeException) ->
-          consoleOut $ "💥 " <> T.pack (show e)
-        Right _ -> pure ()
-      -- shutdown console IO anyway
-      consoleIO console ConsoleShutdown
+      void $
+        forkFinally (edhProgLoop moduSpec console) $ \ !result -> do
+          case result of
+            Left (e :: SomeException) ->
+              consoleOut $ "💥 " <> T.pack (show e)
+            Right _ -> pure ()
+          -- shutdown console IO anyway
+          consoleIO console ConsoleShutdown
 
+      consoleOut ">> Networked Đ (Edh) <<\n"
+      consoleOut
+        "* Blank Screen Syndrome ? Take the Tour as your companion, checkout:\n"
+      consoleOut "  https://github.com/e-wrks/nedh/tree/master/Tour\n"
 
-    consoleOut ">> Networked Đ (Edh) <<\n"
-    consoleOut
-      "* Blank Screen Syndrome ? Take the Tour as your companion, checkout:\n"
-    consoleOut "  https://github.com/e-wrks/nedh/tree/master/Tour\n"
-
-    consoleIOLoop console
+      consoleIOLoop console
