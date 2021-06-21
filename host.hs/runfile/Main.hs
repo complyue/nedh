66a4cf912e862d76b31ba06f68afd6baae64a678
ComplYue <compl.yue@icloud.com>
2021-06-21 20:04:13 +0800

    runfile upd

--- host.hs/runfile/Main.hs
+++ host.hs/runfile/Main.hs
@@ -1,60 +1,22 @@
-
 module Main where
 
-import           Prelude
 -- import           Debug.Trace
 
-import           System.Environment
-import           System.IO
-import           System.Exit
-
-import           Control.Monad
-import           Control.Exception
-import           Control.Concurrent
-
-import qualified Data.Text                     as T
-
-import           Language.Edh.EHI
-import           Language.Edh.Net
-
+import Language.Edh.EHI
+import Language.Edh.Net
+import Language.Edh.Run
+import System.Environment
+import System.Exit
+import System.IO
+import Prelude
 
 main :: IO ()
-main = getArgs >>= \case
-  [edhFile] -> do
+main =
+  getArgs >>= \case
+    [edhFile] -> edhRunFile defaultEdhConsoleSettings edhFile $ \ !world -> do
+      -- install all necessary batteries
+      installEdhBatteries world
+      installNetBatteries world
 
-    !console <- defaultEdhConsole defaultEdhConsoleSettings
-    let !consoleOut = consoleIO console . ConsoleOut
-        runIt       = do
-
-          world <- createEdhWorld console
-          installEdhBatteries world
-
-          -- install batteries provided by nedh
-          installNetBatteries world
-
-          runEdhFile world edhFile >>= \case
-            Left !err -> do
-              -- program crash on error
-              consoleOut "Nedh crashed with an error:\n"
-              consoleOut $ T.pack $ show err <> "\n"
-            Right !phv -> case edhUltimate phv of
-              -- clean program halt, all done
-              EdhNil -> return ()
-              -- unclean program exit
-              _      -> do
-                consoleOut "Nedh halted with a result:\n"
-                consoleOut $ (<> "\n") $ case phv of
-                  EdhString msg -> msg
-                  _             -> T.pack $ show phv
-
-    void $ forkFinally runIt $ \ !result -> do
-      case result of
-        Left (e :: SomeException) ->
-          consoleOut $ "💥 " <> T.pack (show e)
-        Right _ -> pure ()
-      -- shutdown console IO anyway
-      consoleIO console ConsoleShutdown
-
-    consoleIOLoop console
-
-  _ -> hPutStrLn stderr "Usage: runnedh <.edh-file>" >> exitFailure
+    --
+    _ -> hPutStrLn stderr "Usage: runnedh <.edh-file>" >> exitFailure
